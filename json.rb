2c046d2b30bf04c1a91dc237ac2b013068f9279f
Tsuchida Yuki <Yuki@net79-dhcp17.sfc.keio.ac.jp>
2016-11-25 15:54:28 +0900

    画像保存機能の追加

--- json.rb
+++ json.rb
@@ -1,21 +1,43 @@
 require 'json'
 require 'uri'
 require 'net/http'
+require "open-uri"
+require "FileUtils"
+
+def save_image(url)
+  # ready filepath
+  fileName = File.basename(url)
+  dirName = "./img/"
+  filePath = dirName + fileName
+
+  # create folder if not exist
+  FileUtils.mkdir_p(dirName) unless FileTest.exist?(dirName)
+
+  # write image data
+  open(filePath, 'wb') do |output|
+    open(url) do |pic|
+      output.write(pic.read)
+    end
+  end
+end
 
 # 検索タグ
 print 'ユーザー名:'
 USERNAME = gets.chomp
 
 # 取得対象ページ
-url = 'https://www.instagram.com/' + URI.encode_www_form_component(USERNAME) + '/?__a=1'
+feedurl = 'https://www.instagram.com/' + URI.encode_www_form_component(USERNAME) + '/?__a=1'
 
 puts USERNAME + " のデータを取得します"
 
-uri = URI.parse(url)
+uri = URI.parse(feedurl)
 json = Net::HTTP.get(uri)
 result = JSON.parse(json)
 hash = result["user"]["media"]["nodes"]
 hash.each do |data|
-     imgurl = data["thumbnail_src"]  
-    puts imgurl
+     url = data["thumbnail_src"]  
+    url[/\?\S+/] = ""
+   save_image(url)
 end
+
+p "完了しました。"
